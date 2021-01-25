3e1748d799dba3b50b049387b50cce33516239fe
Robin Novak <robin.m.novak@gmail.com>
2021-01-25 11:28:38 -0800

    Express Generator REST API

--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -9,6 +9,34 @@ movieRouter.route('/')
 })
 .get((req, res) => {
     res.end('Will send all movies');
+})
+.post((req, res) => {
+    res.end('Will post movies')
+})
+.put((req, res) => {
+    res.end('Put not accessable on /movies')
+})
+.delete((req, res) => {
+    res.end('Deleting all movies')
+});
+
+movieRouter.route('/:movieId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next()
+})
+.get((req, res) => {
+    res.end(`Sending movie: ${req.params.movieId}`);
+})
+.post((req, res) => {
+    res.end(`Posting movie: ${req.params.movieId}`)
+})
+.put((req, res) => {
+    res.end(`Updating movie: ${req.params.movieId}`)
+})
+.delete((req, res) => {
+    res.end(`Deleting movie: ${req.params.movieId}`)
 });
 
 module.exports = movieRouter;
